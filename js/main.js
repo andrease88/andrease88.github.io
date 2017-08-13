1860ef081a1eb0dd94e2f0ce825f227f378ffd75
pappagnu <andreas@klonad.se>
2017-08-13 11:56:27 +0200

    removed event for scroll in jQuery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    $('body').hide(0).delay(500).fadeIn(2000);
-});
-
-$(".mouse").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".works").offset().top},
-        'slow');
-});
+$(document).ready(function () {
+  $('body').hide(0).delay(500).fadeIn(2000)
+})
